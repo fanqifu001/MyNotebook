3ba1d655fb115e0dc023c12987828cbf2e4477be
fanqifu <fanqifu@longcheer.com>
2024-08-15 18:43:54 +0800

    first day

---
+++ app/src/main/java/com/fqf/mynotebook/db/UserDbHelper.java
@@ -0,0 +1,23 @@
+package com.fqf.mynotebook.db;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class UserDbHelper extends SQLiteOpenHelper {
+    public UserDbHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
