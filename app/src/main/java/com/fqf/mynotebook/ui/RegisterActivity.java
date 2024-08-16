e235665c395c4bf244eac40ee7e7f5733638fb7a
linsan <1789185047@qq.com>
2024-08-16 18:35:11 +0800

    Second day

--- app/src/main/java/com/fqf/mynotebook/ui/RegisterActivity.java
+++ app/src/main/java/com/fqf/mynotebook/ui/RegisterActivity.java
@@ -1,19 +1,14 @@
 package com.fqf.mynotebook.ui;
 
-import android.content.Intent;
-import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
+
 import androidx.appcompat.widget.Toolbar;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+;
 
 import com.fqf.mynotebook.Base.BaseActivity;
 import com.fqf.mynotebook.R;
